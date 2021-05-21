9157fccf6fab756190d05fe9b7d1b18228eecf64
Alfredo M <58049346+acsmarrucho@users.noreply.github.com>
2021-05-21 17:40:07 -0300

    Update scraper_cetip.py

--- scraper_cetip.py
+++ scraper_cetip.py
@@ -5,20 +5,15 @@ import io
 import os
 
 url    = 'https://www2.cetip.com.br/TitulosCRI'
-params = {
-            'txtDtEmissao':"01/01/0001", 
-            'btExportarCSV': "Exportar para CSV"
-            }
-
 s = requests.Session()
 r = s.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
 viewstate  = soup.find('input', {'id': '__VIEWSTATE'})['value']
 validation = soup.find('input', {'id': '__EVENTVALIDATION'})['value']
-r2 = s.post(url, params=params, data={ 
+r2 = s.post(url, data={ 
             '__VIEWSTATE'      : viewstate,
             '__EVENTVALIDATION': validation,
-            'txtDtEmissao':"01/01/2020", 
+            'txtDtEmissao':"01/01/0001", 
             'btExportarCSV': "Exportar para CSV"
         })
 
