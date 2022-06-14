6b1f06e4ae4c7bd59d85d0b8e48400a3928fa88b
Alfredo M <58049346+moderfla@users.noreply.github.com>
2022-06-14 20:21:28 -0300

    Update scraper_cetip.py

--- scraper_cetip.py
+++ scraper_cetip.py
@@ -18,4 +18,4 @@ headers = {
 response = requests.get('https://sistemaswebb3-balcao.b3.com.br/featuresCRIProxy/CriCall/GetDownload', headers=headers, verify=False)
 tab = base64.b64decode(response.text).decode("ISO-8859-1")
 cri = pd.read_csv(io.StringIO(tab), delimiter=";", encoding='ISO-8859-1', parse_dates=[8, 9, 10], infer_datetime_format= 'True', dayfirst='True', thousands=".", decimal=',')
-cri.to_csv('emissoes_cri.csv', index=False)
+cri.drop(cri.columns[[12]], axis=1).to_csv('emissoes_cri.csv', index=False)
